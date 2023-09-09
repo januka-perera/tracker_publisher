2d8c73e520286f44bec77701dfba7ff01125c7a0
Januka Perera <pererajanuka2000@gmail.com>
2023-09-09 23:46:32 +1000

    Adding some plots after filtering

--- src/tracker_info.py
+++ src/tracker_info.py
@@ -3,7 +3,7 @@ from prettify_plot import prettify_plot
 from vector_field import draw_vector_plot
 import time
 if __name__ == "__main__":
-    dataset_directory = "2d_csvs"
+    dataset_directory = "filtered_csv"
     t = time.localtime()
     current_time = time.strftime("%H:%M:%S", t)
     build_heatmap(csv_directory=dataset_directory, x_pos_label="x_pos", y_pos_label="y_pos", cell_size=0.5)
